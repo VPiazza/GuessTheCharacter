6f20ab219952798c2665870c37b31a93e0b79eca
VPiazza <learner-362d5e@extensionschool.ch>
2020-04-25 15:46:00 +0200

    JS completed and simplified. Py to be written.

--- randomCoupling.js
+++ randomCoupling.js
@@ -1,46 +1,60 @@
-var partecipants =["Valerio", "Giuliana", "Bernardo", "Gloria"];
-var topic = ["Disney","Oggetti di Casa", "Mestieri"];
+function topicChoice(topic) {
+    var cat = Math.floor(Math.random()*(topic.length));
+    var msg = "In questo turno l'argomento è " + topic[cat];
+    topic.splice(cat,1);
+    return console.log(msg);
+}
+
+function startingSets(partecipants) {
+    var chooser = [];
+    var chosen = [];
+    var P1 = [];
+    var P2 = [];
+    for (i = 0; i < partecipants.length; i+=1) {
+        chooser.push(partecipants[i]);
+        chosen.push(partecipants[i]);
+    }
+    return [chooser, chosen, P1, P2]
+}
 
 function randomCoupl(partecipants) {
     var chooser = [];
     var chosen = [];
-    for (i = 0; i < partecipants.length; i+=1) {
-        chooser.push(partecipants[i]);
-        chosen.push(partecipants[i]);
-    } 
-    person1 = [];
-    person2 = [];
+    var P1 = [];
+    var P2 = [];
+    [chooser, chosen, P1, P2] = startingSets(partecipants);
     while (chooser.length > 0) {
         var a = Math.floor(Math.random()*chooser.length);
         var b = Math.floor(Math.random()*chosen.length);
         if (chooser[a] != chosen[b]) {
-            person1.push(chooser[a]);
-            person2.push(chosen[b]);
-            chooser.splice (a,1);
+            P1.push(chooser[a]);
+            P2.push(chosen[b]);
+            chooser.splice(a,1);
             chosen.splice(b,1);
         } else {
-            person1 = [];
-            person2 = [];
-            var chooser = [];
-            var chosen = [];
-            for (i = 0; i < partecipants.length; i+=1) {
-                chooser.push(partecipants[i]);
-                chosen.push(partecipants[i]);
+            for (i=0; i < P1.length; i +=1) {
+                chooser.push(P1[i]);
+                chosen.push(P2[i]);
+                P1.splice(i,1);
+                P2.splice(i,1);
             }
         }
     }
-    for (i=0; i < person1.length; i+=1) { 
-        console.log(person1[i] + " sceglie il personaggio per " + person2[i]);
+    for (i=0; i < P1.length; i+=1) { 
+        console.log(P1[i] + " sceglie il personaggio per " + P2[i]);
     }
 }
 
 
+var partecipants = ["Valerio", "Giuliana", "Bernardo", "Gloria"];
+var topic = ["Storia", "Film", "Cartoni", "Serie TV", "Disney","Oggetti di Casa", "Mestieri"];
+
+
+//  Running script //
 while (topic.length > 0) {
-    var cat = Math.floor(Math.random()*(topic.length));
-    console.log ("In questo turno l'argomento è " + topic[cat]);
+    topicChoice(topic);
     randomCoupl(partecipants);
+    console.log("           ");
     window.prompt("Premi ok per iniziare il prossimo round!");
-    topic.splice(cat,1);
-    
-}  
+}
 
